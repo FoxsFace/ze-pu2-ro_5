8ef3c31935825335cb58b137b93a6d30e4336c88
Dudko Alexandr <65069828+FoxsFace@users.noreply.github.com>
2022-11-15 22:00:29 +0300

    Add files via upload

---
+++ kkk/js/knopka.js
@@ -0,0 +1,7 @@
+var a = document.querySelector('.c-button');
+a.onclick = function () {
+    var mapp = document.querySelector('#map');
+    mapp.classList.add("a");
+    coslole.log("hf,jnftn ,kznm");
+}
+
